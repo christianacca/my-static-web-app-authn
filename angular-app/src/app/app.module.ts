96bcff84d6cfd3108d508db47db0ad81c1705ae0
Christian Crowhurst <christian.crowhurst@mrisoftware.com>
2021-08-14 07:13:40 +0100

    configuring custom idp + managed idp's not supported by swa's :-(

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-import {RouterModule} from '@angular/router';
 
 import {routes} from './router';
 import {AppComponent} from './app.component';
 import {AppStoreModule} from './store/store.module';
 import {AboutComponent} from './about.component';
+import {RouterModule} from '@angular/router';
 import {externalModules} from './build-specific';
 import {AuthModule, declarations} from './core';
+
+const auth0Idp = { key: 'auth0', name: 'Auth0'};
 
 @NgModule({
   declarations: [AppComponent, AboutComponent, declarations],
@@ -18,7 +20,10 @@ import {AuthModule, declarations} from './core';
     RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
     AppStoreModule,
     externalModules,
-    AuthModule.forRoot()
+    AuthModule.forRoot({
+      availableIdentityProviders: [auth0Idp],
+      defaultIdentityProviderKey: auth0Idp.key
+    })
   ],
   bootstrap: [AppComponent]
 })
